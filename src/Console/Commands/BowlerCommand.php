6cc1127144c34daef5b2fd138f144787fa3a804c
Ali Issa <alisissa@gmail.com>
2016-09-08 14:45:43 +0300

    updates

--- src/Console/Commands/BowlerCommand.php
+++ src/Console/Commands/BowlerCommand.php
@@ -6,12 +6,13 @@ use Vinelab\Bowler\RegisterQueues;
 use Vinelab\Bowler\Consumer;
 use Vinelab\Bowler\Connection;
 
-use Illuminate\Console\Command;
+//use Illuminate\Console\Command;
+use App\Commands\Command;
 
 /**
  * @author Ali Issa <ali@vinelab.com>
  */
-class BowlerCommand extends Command
+class BowlerCommand extends Command implements SelfHandling
 {
     protected $registerQueues;
     //protected $consumer;
