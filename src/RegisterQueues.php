74a07c5e51100df837b8309d0f18ad56b54df684
Kinane <kinane.domloje@gmail.com>
2017-01-16 15:57:36 +0200

    set handler's options

--- src/RegisterQueues.php
+++ src/RegisterQueues.php
@@ -2,18 +2,22 @@
 
 namespace Vinelab\Bowler;
 
-use Vinelab\Bowler\Handler;
-
+/**
+ * @author Ali Issa <ali@vinelab.com>
+ * @author Kinane Domloje <kinane@vinelab.com>
+ */
 class RegisterQueues
 {
     private $handlers = [];
 
     public function queue($queue, $className, $options = [])
     {
-    	$handler = new Handler();
-    	$handler->queueName = $queue;
-    	$handler->className = $className;
-      	array_push($this->handlers, $handler);
+        $handler = new Handler();
+        $handler->queueName = $queue;
+        $handler->className = $className;
+        $handler->options = $options;
+
+        array_push($this->handlers, $handler);
     }
 
     public function getHandlers()
