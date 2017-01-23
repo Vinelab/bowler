b30200631ec5867652f32e8d410550ef7969466e
Kinane <kinane.domloje@gmail.com>
2017-01-23 18:58:36 +0200

    Fix Exception

--- src/Exceptions/UnregisteredQueueException.php
+++ src/Exceptions/UnregisteredQueueException.php
@@ -1,13 +1,18 @@
 <?php
 
-use Vinelab\Bowler\BowlerGeneralException;
+namespace Vinelab\Bowler\Exceptions;
+
+use Vinelab\Bowler\Exceptions\BowlerGeneralException;
 
 /**
  * @author Kinane Domloje <kinane@vinelab.com>
  */
 class UnregisteredQueueException extends BowlerGeneralException
 {
-    public function __construct()
+    protected $message;
+
+    public function __construct($message)
     {
+        $this->message = $message;
     }
 }
