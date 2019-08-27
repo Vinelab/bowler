5aaf4dfbaaee17ed7b7bbc807e28d7dd9ee7aba9
Alexander Diachenko <adiach3nko@gmail.com>
2019-08-27 10:18:55 +0300

    Reorder config

--- config/bowler.php
+++ config/bowler.php
@@ -2,16 +2,6 @@
 
 return [
 
-    'log' => [
-        'message' => [
-            'truncate_length' => 16000,
-        ],
-    ],
-
-    'lifecycle_hooks' => [
-        'fail_on_error' => false,
-    ],
-
     'rabbitmq' => [
         'host' => trim(env('RABBITMQ_HOST', 'localhost')),
         'port' => trim(env('RABBITMQ_PORT', '5672')),
@@ -20,7 +10,17 @@ return [
         'connection_timeout' => 60,
         'read_write_timeout' => 60,
         'heartbeat' => 30,
-        'vhost' => '/'
+        'vhost' => '/',
+    ],
+
+    'lifecycle_hooks' => [
+        'fail_on_error' => false,
+    ],
+
+    'log' => [
+        'message' => [
+            'truncate_length' => 16000,
+        ],
     ],
 
 ];
